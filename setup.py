8e7b40bfc15b5a81919306e9475070e41004894d
Robin Harms Oredsson <robin@betahaus.net>
2017-11-24 21:37:20 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_require = (
 
 setup(
     name='STVPoll',
-    version='0.1.2',
+    version='0.1.3',
     description='STV polling methods',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
